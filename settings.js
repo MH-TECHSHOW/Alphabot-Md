cebfb1c5688caed4c6468dc97e8fde5dfd008830
MH-TECHSHOW <103843668+MH-TECHSHOW@users.noreply.github.com>
2023-02-09 11:48:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ global.APIKeys = {
 // setting 
 global.autoread = true // auto read message
 global.autobio = false //auto ganti bio
-global.anticall = true //anti call 
+global.anticall = false //anti call 
 global.userRegister = false
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 
@@ -31,8 +31,8 @@ global.language = english //change indonesia to english if you don't understand 
 
 // Other
 // Other
-global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
+global.botname = "𓄂𝙳𝙷𝙰𝙽𝚄𝚆𝙰ᥫ᭡⚘-Md" //namabot kalian
+global.ownername = "𓄂𝙳𝙷𝙰𝙽𝚄𝚆𝙰ᥫ᭡⚘" //nama kalian
 global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
 global.myweb2 = "https://s.id/BOTGITHUB" //bebas
@@ -41,8 +41,8 @@ global.region = "Buddhist" //bebas
 global.timezone = 'Asia/Colombo' //  timezone wib
 global.premium = ['94764497078'] //premium user
 global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
-global.ownernomerr = "+94764497078" //nmr wa kalian
+global.ownernomer = "94768911863" // nomor wa kalian
+global.ownernomerr = "+94768911863" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
